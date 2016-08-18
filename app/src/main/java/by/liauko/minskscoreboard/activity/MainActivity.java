d6a84707c7484897c7f782b5d5f7f13b35d84305
Sergey Levko <okvel.s@gmail.com>
2016-08-18 22:52:59 +0300

    Fixed project structure. NavigationView was implemented. Home navigation item action was implemented.

--- app/src/main/java/by/liauko/minskscoreboard/activity/MainActivity.java
+++ app/src/main/java/by/liauko/minskscoreboard/activity/MainActivity.java
@@ -1,16 +1,22 @@
 package by.liauko.minskscoreboard.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import by.liauko.minskscoreboard.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     private Toolbar mToolbar;
     private WebView mWebView;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initToolbar();
+        initNavigationView();
         initWebView();
     }
 
@@ -27,10 +34,38 @@ public class MainActivity extends AppCompatActivity {
         mToolbar.setTitle(getString(R.string.app_name));
     }
 
+    private void initNavigationView() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
+                R.string.view_navigation_open, R.string.view_navigation_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
     private void initWebView() {
         mWebView = (WebView) findViewById(R.id.web_view);
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new WebViewClient());
+        loadHomePage();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        boolean result = false;
+        switch (item.getItemId()) {
+            case R.id.home_menu_nav_item:
+                loadHomePage();
+                result = true;
+        }
+        mDrawerLayout.closeDrawers();
+
+        return result;
+    }
+
+    private void loadHomePage() {
         mWebView.loadUrl("http://www.minsktrans.by/lookout_yard/");
     }
 }
