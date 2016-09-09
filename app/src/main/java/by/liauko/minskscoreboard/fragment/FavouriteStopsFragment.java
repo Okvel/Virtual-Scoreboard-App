42fc08a5a7339182c20c37e3b9dc0e949aea3828
Sergey Levko <okvel.s@gmail.com>
2016-09-09 20:36:46 +0300

    Add fragment which displayed user's list of favourite stops.

---
+++ app/src/main/java/by/liauko/minskscoreboard/fragment/FavouriteStopsFragment.java
@@ -0,0 +1,18 @@
+package by.liauko.minskscoreboard.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import by.liauko.minskscoreboard.R;
+
+public class FavouriteStopsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.favourite_stops_fragment, null);
+    }
+}
